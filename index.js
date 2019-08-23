9224b30969dfd4b86627a20dca0414e64ea58610
Hamidou Diallo <dio.hamidou@gmail.com>
2019-08-23 04:02:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const whisper = string =>{
   return string.toLowerCase();
 };
 
+// test 3
 const logShout = string =>{
   console.log(string.toUpperCase());
+};
+
+// test 4
+const whisperShout = string =>{
+  console.log(string.toLowerCase());
 };
